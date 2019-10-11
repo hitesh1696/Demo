61fee96e326df6fa67e1e7b11922c316fa22c0d3
hitesh1696 <hitesh.patil1696@gmail.com>
2019-10-12 00:18:33 +0530

    My second commit

---
+++ demo/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Tutorial
+from django.contrib.auth.forms import UserCreationForm
+# Create your views here.
+def homepage(request):
+    return render(request=request,
+                  template_name="main/homepage.html",
+                  context={"tutorials":Tutorial.objects.all})
+def home(request):
+    return render(request=request,
+                  template_name="main/home.html",
+                  context={"tutorials":Tutorial.objects.all})
+def register(request):
+    form = UserCreationForm
+    return render(request,
+                  "main/register.html",
+                  context={"form":form})
